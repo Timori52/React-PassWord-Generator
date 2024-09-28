00ef621e3891fd9a182e2205c5c3360e62c9598c
sumit timori <timori52@gmail.com>
2024-09-28 14:53:09 +0530

    added copyToClipboard fn

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState, useEffect } from "react";
+import { useCallback, useState, useEffect, useRef } from "react";
 
 function App() {
   const [length, setLength] = useState(8);
@@ -12,20 +12,29 @@ function App() {
     if (number) str += "1234567890";
     if (character) str += ")(*&^%$#@!`~";
 
-    for (let i = 1; i <= str.length; i++) {
-      let char = (Math.random() * str.length);
+    for (let i = 1; i <= length; i++) {
+      let char = (Math.random() * str.length + 1);
       pass += str.charAt(char);
+      
     }
     setPassword(pass);
-  }, [number, character]);
+  }, [number, character, length]);
 
   useEffect(() => {
     passGenerator();
   }, [number, character, length, passGenerator]);
 
+   const pwdRef =useRef(null);
+   
+
+  const  copyToClipboard = () => {
+    pwdRef.current?.select();
+    window.navigator.clipboard.writeText(password);
+  }
+
   return (
     <>
-      <div className=" w-full max-w-md mx-auto shadow-2xl rounded-lg my-8 px-4  py-3  text-orange-500 bg-gray-700 text-center">
+      <div className=" w-full max-w-lg mx-auto shadow-2xl rounded-lg my-8 px-4  py-3  text-orange-500 bg-gray-700 text-center">
         <h1 className="mb-4 text-center">Password Generator</h1>
         <div className="flex rounded-xl shadow overflow-hidden mb-4">
           <input
@@ -34,13 +43,23 @@ function App() {
             placeholder="PWD shown here"
             readOnly
             value={password}
+            ref={pwdRef}
           />
           <button
             type="button"
-            className="px-4 bg-orange-500 text-white  hover:bg-orange-400"
-            onClick={passGenerator}
+            className="px-4 bg-blue-600 text-white  hover:bg-orange-700"
+            onClick={copyToClipboard}
           >
             Copy
+          </button>
+          <button
+            type="button"
+            className="px-4 bg-orange-500 text-white  hover:bg-orange-700 shrink-0" 
+            onClick={passGenerator}
+
+
+          >
+            Generate New Password
           </button>
         </div>
         <div className="flex gap-x-2 text-sm text-center">
@@ -51,6 +70,7 @@ function App() {
               min={6}
               max={100}
               value={length}
+              
               onChange={(e) => setLength(e.target.value)}
             />
             <label> length : {length}</label>
